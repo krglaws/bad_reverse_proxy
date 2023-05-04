988ce98f532d4f922e3847b4cf6e3d02d816412e
Kyle Glaws <kyle.glaws@gmail.com>
2023-05-03 20:28:43 -0400

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# maps hostname to another server/port combo in a tuple
+# example: "www.example.com": ('internal_hostname', <port>)
+HOST_MAP = {}
+
+# redirect url if target host is not found in HOST_MAP
+HNF_REDIRECT = ''
+
+# recv buffer size
+BUFSIZE = 1024 * 1024
+
